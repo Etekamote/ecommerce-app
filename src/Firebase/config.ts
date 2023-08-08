c45c3fcbda55433140021f85cefc8570fa814642
Kacper <kacpermaciejewski1998@gmail.com>
2023-08-08 18:32:55 +0200

    test commit

---
+++ src/Firebase/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuD2p40hJ7rIOTYkkh7SWuihQk4FAY41k",
+  authDomain: "imadeitup-543cd.firebaseapp.com",
+  projectId: "imadeitup-543cd",
+  storageBucket: "imadeitup-543cd.appspot.com",
+  messagingSenderId: "437214839808",
+  appId: "1:437214839808:web:3e799a6735d9c39d32c4e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

ecbbb97f7c62c0c6e02c16bc2fc27f806625f0be
Kacper <kacpermaciejewski1998@gmail.com>
2023-09-03 19:13:21 +0200

    created randomproducts component and added shuffle function for array of products

--- src/Components/Home/randomproducts.tsx
+++ src/Components/Home/randomproducts.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useProducts } from '../../hooks/useProducts'
+import { ProductInterface } from '../../interfaces/ProductInterface';
 
-export const Randomproducts = () => {
+export const RandomProducts = ({category}:{category: string}) => {
+
+    const shuffle = (array: ProductInterface[]) => { 
+        for (let i = array.length - 1; i > 0; i--) { 
+          const j = Math.floor(Math.random() * (i + 1)); 
+          [array[i], array[j]] = [array[j], array[i]]; 
+        } 
+        return array; 
+      }; 
+
+
+    const products = shuffle(useProducts(category))
+    console.log(products)
+    
   return (
-    <div>R</div>
+    <div>Hi!</div>
   )
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Hero } from '../Components/Home/hero'
 import { styled } from 'styled-components'
+import { RandomProducts } from '../Components/Home/randomproducts'
 
 
 const StyledMain = styled.main`
@@ -12,6 +13,7 @@ export const Home = () => {
   return (
     <StyledMain>
       <Hero />
+      <RandomProducts category="men" />
       </StyledMain>
       
     
